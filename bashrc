8c5bc836d629248d4daca52824b13d225ea21ce5
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2025-01-07 14:30:25 +0000

    update: intall script

--- bashrc
+++ bashrc
@@ -1,3 +1,7 @@
+
+# shell color
+PS1='(w)\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+
 export WTOOLSET_DIR=$(pwd)
 export XDG_CONFIG_HOME=$WTOOLSET_DIR/config/
 export PATH=$WTOOLSET_DIR/bin/:$PATH
@@ -7,8 +11,15 @@ export NVM_DIR="$XDG_CONFIG_HOME/nvm"
 [ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"
 command -v yarn >/dev/null 2>&1 || {
   echo "yarn not found. try installing..."
-  npm install -g yarn
+  if command -v npm >/dev/null 2>&1
+  then
+    npm install -g yarn
+  else
+    echo "npm not found. please install node first and rerun the shell"
+  fi
 }
 
 alias vi='nvim'
 alias lg='lazygit'
+
+
--- install.sh
+++ install.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+set -e
+
+# install nvm & node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+nvm install --lts
+source ./bashrc
+
+# install fd
+wget https://github.com/sharkdp/fd/releases/download/v10.2.0/fd-v10.2.0-x86_64-unknown-linux-musl.tar.gz
+tar xf fd-v10.2.0-x86_64-unknown-linux-musl.tar.gz --strip-components=1 fd-v10.2.0-x86_64-unknown-linux-musl/fd
+mv fd bin/fd
+
+# install ripgrep
+wget https://github.com/BurntSushi/ripgrep/releases/download/14.1.1/ripgrep-14.1.1-x86_64-unknown-linux-musl.tar.gz
+tar xf ripgrep-14.1.1-x86_64-unknown-linux-musl.tar.gz --strip-components=1 ripgrep-14.1.1-x86_64-unknown-linux-musl/rg
+mv rg bin/rg
+
+# install nvim
