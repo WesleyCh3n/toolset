dbaf77d851994540a6e4d956d109a15eccf2fa44
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2025-01-08 02:56:21 +0000

    refactor: install binary

--- install.sh
+++ install.sh
@@ -1,19 +1,52 @@
 #!/bin/bash
-set -e
+set -euo pipefail
 
-# install nvm & node
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-nvm install --lts
-source ./bashrc
+BIN_DIR="bin"
 
-# install fd
-wget https://github.com/sharkdp/fd/releases/download/v10.2.0/fd-v10.2.0-x86_64-unknown-linux-musl.tar.gz
-tar xf fd-v10.2.0-x86_64-unknown-linux-musl.tar.gz --strip-components=1 fd-v10.2.0-x86_64-unknown-linux-musl/fd
-mv fd bin/fd
+# Function to install a binary from a tar.gz release
+install_tool() {
+  local name=$1 # binary name
+  local version=$2
+  local url_template=$3
+  local path_template=$4
 
-# install ripgrep
-wget https://github.com/BurntSushi/ripgrep/releases/download/14.1.1/ripgrep-14.1.1-x86_64-unknown-linux-musl.tar.gz
-tar xf ripgrep-14.1.1-x86_64-unknown-linux-musl.tar.gz --strip-components=1 ripgrep-14.1.1-x86_64-unknown-linux-musl/rg
-mv rg bin/rg
+  echo "Installing $name version $version..."
 
-# install nvim
+  local url=$(echo "$url_template" | sed "s/{{VERSION}}/$version/g")
+  local path=$(echo "$path_template" | sed "s/{{VERSION}}/$version/g")
+  local tar_file="${name}-${version}.tar.gz"
+
+  curl -# -L "$url" -o "$tar_file"
+
+  tar xf "$tar_file" \
+    --strip-components=$(echo "$path" | tr -cd '/' | wc -c) \
+    "$path"
+  mv "$name" "$BIN_DIR/$name"
+  rm "$tar_file"
+
+  echo "$name installed successfully."
+}
+
+main() {
+  # install nvm & node
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+  source ./bashrc
+  nvm install --lts
+
+  install_tool "fd" "10.2.0" \
+    "https://github.com/sharkdp/fd/releases/download/v{{VERSION}}/fd-v{{VERSION}}-x86_64-unknown-linux-musl.tar.gz" \
+    "fd-v{{VERSION}}-x86_64-unknown-linux-musl/fd"
+
+  install_tool "rg" "14.1.1" \
+    "https://github.com/BurntSushi/ripgrep/releases/download/{{VERSION}}/ripgrep-{{VERSION}}-x86_64-unknown-linux-musl.tar.gz" \
+    "ripgrep-{{VERSION}}-x86_64-unknown-linux-musl/rg"
+
+  install_tool "lazygit" "0.44.1" \
+    "https://github.com/jesseduffield/lazygit/releases/download/v{{VERSION}}/lazygit_{{VERSION}}_Linux_x86_64.tar.gz" \
+    "lazygit"
+
+  curl -# -LO "https://github.com/neovim/neovim/releases/download/v0.10.3/nvim-linux64.tar.gz"
+  tar xf nvim-linux64.tar.gz --strip-components=1
+}
+
+main
